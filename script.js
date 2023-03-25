36b959d0c01189a4cc7f7a436bec45b088fcac9f
Aaryan Chaudhary <aaryanchaudhary188@gmail.com>
2023-03-25 17:28:01 +1100

    completed input validation

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const button = document.querySelector(".add");
 const section = document.querySelector(".section");
 const input = document.querySelector(".input");
+const container = document.querySelector(".container");
+const errorMessage = document.querySelector("#error-message");
+
+const taskArray = [];
+
+const storeTask = () => {
+  taskArray.push(input.value);
+  console.log(taskArray);
+};
 
 button.addEventListener("click", (e) => {
   e.preventDefault();
-
-  section.innerHTML += `<div class= "card">
-  <h1>${input.value}</h1>
-  </div>`;
-  input.value = " ";
+  if (input.value.trim() !== "") {
+    section.innerHTML += `<div class= "card">
+      <h1>${input.value}</h1> <button>delete</button>
+    </div>`;
+    storeTask();
+    input.value = "";
+    errorMessage.innerHTML = "";
+  } else {
+    errorMessage.innerHTML = "Please enter a task.";
+  }
 });
